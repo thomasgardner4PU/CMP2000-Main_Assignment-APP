d6a4b5395ea7c1dd7b348ae0e43b0c09d0f4193d
thomasgardner4PU <72255127+thomasgardner4PU@users.noreply.github.com>
2020-12-23 01:25:42 +0000

    Commit 1 - Created GUI form, named main JPanel and set dimensions for project App.

--- src/AutomatedCheckOutSystem.java
+++ src/AutomatedCheckOutSystem.java
@@ -1,4 +1,19 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class AutomatedCheckOutSystem extends JFrame {
+
+    private JPanel mainPanel;
+
+    public static void main(String[] args ) {
+        AutomatedCheckOutSystem Page = new AutomatedCheckOutSystem("loading from file");
+        Page.setVisible(true);
+    }
+
+    public AutomatedCheckOutSystem(String loading_from_file) {
+        setContentPane(mainPanel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(500,500));
+        pack();
+    }
 }
