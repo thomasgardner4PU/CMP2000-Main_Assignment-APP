5d16ec29404e65f880989e48910f892282eb86a7
tom <thomas.gardner-1@studnets.plymouth.ac.uk>
2020-12-29 17:46:57 +0000

    Changes made:
    AdminPage menu now has buttons for scan and payment method.

--- src/AdminPage.java
+++ src/AdminPage.java
@@ -3,4 +3,9 @@ import javax.swing.*;
 public class AdminPage extends JFrame {
 
     private JPanel AdminPanel;
+    private JLabel WelcomeLbl;
+    private JPanel ScanPage;
+    private JPanel PaymentPage;
+    private JButton ScanItemsBtn;
+    private JButton Payment;
 }
